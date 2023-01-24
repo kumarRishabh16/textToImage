c200614f5583cd4a719587501b7ac6f274ac2e19
kumarRishabh16 <rishabhk.btech2018@iujaipur.edu.in>
2023-01-24 16:29:20 +0530

    ip removed

--- chatgptproject1/app/chatgptfunction.py
+++ chatgptproject1/app/chatgptfunction.py
@@ -10,7 +10,7 @@ import requests
 
 def generate_image_from_prompt(prompt):
     # Replace YOUR_API_KEY with your actual API key
-    api_key = 'your api key'
+    api_key = 'your_api_key'
 
     # The API endpoint for the DALL-E API
     url = 'https://api.openai.com/v1/images/generations'
--- chatgptproject1/chatgptproject1/settings.py
+++ chatgptproject1/chatgptproject1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-v7jwnw)_mtgx48$ot^90)@saonvi-t($m_w+=cf1@%111_@eus
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.8.204"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
